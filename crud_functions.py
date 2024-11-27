095bd7cd5f6f6184c895e3b0adcdb63c4c782170
Alex <alivsaz@gmail.com>
2024-11-27 13:01:27 +0300

    init module_14_5

--- crud_functions.py
+++ crud_functions.py
@@ -1,6 +1,8 @@
+from config import db   # файл с настройками
 import sqlite3
 
-def initiate_db(db):
+def initiate_db():
+    global db
     conn = sqlite3.connect(db)
     cursor = conn.cursor()
     cursor.execute(
@@ -13,18 +15,44 @@ def initiate_db(db):
     '''
     )
 
-    # for i in range(4):
-    #     cursor.execute('INSERT INTO Products (id, title, description, price) VALUES(?, ?, ?, ?)',
-    #                (f'{i+1}', f'Продукт {i+1}', f'Описание {i+1}', f'{(i+1) * 100}'))
+    cursor.execute(
+    '''
+    CREATE TABLE IF NOT EXISTS Users (
+    id INTEGER PRIMARY KEY, 
+    username TEXT NOT NULL, 
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL)
+    '''
+    )
 
     conn.commit()
     conn.close()
 
-def get_all_products(db):
+def get_all_products():
+    global db
     conn = sqlite3.connect(db)
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM Products')
     return cursor.fetchall()
     conn.close()
 
+def add_user(username, email, age):
+    global db
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES(?, ?, ?, ?)',
+                    (username, email, age, 1000))
+    conn.commit()
+    conn.close()
 
+def is_included(username):
+    global db
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+    if cursor.execute('SELECT COUNT(*) FROM Users WHERE username = ?', (username,)).fetchone()[0]:
+        conn.close()
+        return True
+    else:
+        conn.close()
+        return False
